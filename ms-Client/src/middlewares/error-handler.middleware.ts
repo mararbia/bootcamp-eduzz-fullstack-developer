b8286cf742d009afe007b1dbb1f209d9ee3b4aa6
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-01-27 14:28:46 -0300

    add basic authentication for login jwt generate

--- ms-Client/src/middlewares/error-handler.middleware.ts
+++ ms-Client/src/middlewares/error-handler.middleware.ts
@@ -1,13 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import DatabaseError from "../models/errors/database.error.model";
+import ForbiddenError from "../models/errors/forbidden.error.models";
 
-function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
-    if (error instanceof DatabaseError) {
-        res.sendStatus(StatusCodes.BAD_REQUEST);
-      } else {
-        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-      }
+function errorHandler(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof DatabaseError) {
+    res.sendStatus(StatusCodes.BAD_REQUEST);
+  } else if (error instanceof ForbiddenError) {
+    res.sendStatus(StatusCodes.FORBIDDEN);
+  } else {
+    res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
 }
 
 export default errorHandler;
